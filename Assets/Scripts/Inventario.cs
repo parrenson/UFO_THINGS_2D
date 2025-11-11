b6d053b61716d1e559ba894e6fbd08213753d00b
Juangalindo27 <jgalindomina51@gmail.com>
2025-11-11 16:47:08 -0500

    Avance Interacciones 2

--- Assets/Scripts/Inventario.cs
+++ Assets/Scripts/Inventario.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Inventario : MonoBehaviour
 {
@@ -7,11 +8,17 @@ public class Inventario : MonoBehaviour
     public Image linternaIcono;
     public Image bateriaIcono;
     public Image curacionIcono;
+    public TMP_Text Bateriatexto;
+    public TMP_Text Curaciontexto;
 
     [Header("Estado del Inventario")]
     public bool tieneLinterna = false;
     public int baterias = 0;
     public int curaciones = 0;
+
+    [Header("Referencias externas")]
+    public LinternaController linterna; // Asignar en Inspector
+    public PlayerController jugador;    // Asignar en Inspector
 
     void Start()
     {
@@ -35,10 +42,51 @@ public class Inventario : MonoBehaviour
         ActualizarInventario();
     }
 
+    public void UsarLinterna()
+    {
+        if (tieneLinterna)
+        {
+            linterna.ToggleLinterna();
+        }
+    }
+
+    public void UsarCuracion()
+    {
+        if (curaciones > 0)
+        {
+            curaciones--;
+            jugador.Curar(20); // +20 HP por ejemplo
+            ActualizarInventario();
+        }
+    }
+
+    public bool UsarBateria()
+    {
+        if (baterias > 0)
+        {
+            baterias--;
+            linterna.RecargarBateria();
+            ActualizarInventario();
+            return true;
+        }
+        return false;
+    }
+
+    void Update()
+    {
+        // Teclas rápidas de uso
+        if (Input.GetKeyDown(KeyCode.F)) UsarLinterna();
+        if (Input.GetKeyDown(KeyCode.H)) UsarCuracion();
+        if (Input.GetKeyDown(KeyCode.R)) UsarBateria();
+    }
+
     void ActualizarInventario()
     {
         linternaIcono.color = tieneLinterna ? Color.white : new Color(1, 1, 1, 0.3f);
         bateriaIcono.color = baterias > 0 ? Color.white : new Color(1, 1, 1, 0.3f);
         curacionIcono.color = curaciones > 0 ? Color.white : new Color(1, 1, 1, 0.3f);
+
+        Bateriatexto.text = "x" + baterias;
+        Curaciontexto.text = "x" + curaciones;
     }
 }
