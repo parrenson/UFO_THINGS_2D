9871f3be8d1e94002791761c137e2bd832e933f5
avaluauto <soporte@avaluauto.com>
2025-11-18 21:36:26 -0500

    FIX: VISTA DE VIDAS - Jugador

--- Assets/Scripts/Movimiento top down.cs
+++ Assets/Scripts/Movimiento top down.cs
@@ -7,6 +7,8 @@ public class Movimientotopdown : MonoBehaviour
     [SerializeField] private float velocidadMovimiento = 5f;
     [SerializeField] private Vector2 direccion;
 
+    private Vector2 direccionMirada = Vector2.up;
+
     private Rigidbody2D rb2D;
 
     [Header("Sonido de pasos")]
@@ -21,17 +23,16 @@ public class Movimientotopdown : MonoBehaviour
         audioSource.playOnAwake = false;
         audioSource.clip = footstepClip;
         audioSource.loop = true;
-        audioSource.spatialBlend = 0f; 
+        audioSource.spatialBlend = 0f;
         audioSource.volume = 0.6f;
         audioSource.priority = 256;
     }
 
-
     private void Update()
     {
-        direccion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
-        bool estaCaminando = direccion.magnitude > 0;
+        bool estaCaminando = input.magnitude > 0;
 
         if (estaCaminando && !audioSource.isPlaying)
         {
@@ -41,10 +42,27 @@ public class Movimientotopdown : MonoBehaviour
         {
             audioSource.Stop();
         }
+
+        if (input.magnitude > 0)
+        {
+            direccion = input;
+            direccionMirada = input;
+        }
+        else
+        {
+            direccion = Vector2.zero;
+        }
     }
 
     private void FixedUpdate()
     {
         rb2D.MovePosition(rb2D.position + direccion * velocidadMovimiento * Time.deltaTime);
+
+        if (direccionMirada != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(direccionMirada.y, direccionMirada.x) * Mathf.Rad2Deg - 90f;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
     }
+
 }
