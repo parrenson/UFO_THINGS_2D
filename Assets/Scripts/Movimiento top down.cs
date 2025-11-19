2e29339b16e834922b7dd1bb845fafc382454e63
SGiraldo017 <sebastian.giraldo_q@uao.edu.co>
2025-11-19 00:17:59 -0500

    ad
    asd

--- Assets/Scripts/Movimiento top down.cs
+++ Assets/Scripts/Movimiento top down.cs
@@ -7,9 +7,8 @@ public class Movimientotopdown : MonoBehaviour
     [SerializeField] private float velocidadMovimiento = 5f;
     [SerializeField] private Vector2 direccion;
 
-    private Vector2 direccionMirada = Vector2.up;
-
     private Rigidbody2D rb2D;
+
     [Header("Sonido de pasos")]
     public AudioClip footstepClip;
     private AudioSource audioSource;
@@ -27,11 +26,12 @@ public class Movimientotopdown : MonoBehaviour
         audioSource.priority = 256;
     }
 
+
     private void Update()
     {
-        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        direccion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
 
-        bool estaCaminando = input.magnitude > 0;
+        bool estaCaminando = direccion.magnitude > 0;
 
         if (estaCaminando && !audioSource.isPlaying)
         {
@@ -41,27 +41,10 @@ public class Movimientotopdown : MonoBehaviour
         {
             audioSource.Stop();
         }
-
-        if (input.magnitude > 0)
-        {
-            direccion = input;
-            direccionMirada = input;
-        }
-        else
-        {
-            direccion = Vector2.zero;
-        }
     }
 
     private void FixedUpdate()
     {
         rb2D.MovePosition(rb2D.position + direccion * velocidadMovimiento * Time.deltaTime);
-
-        if (direccionMirada != Vector2.zero)
-        {
-            float angle = Mathf.Atan2(direccionMirada.y, direccionMirada.x) * Mathf.Rad2Deg - 90f;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-        }
     }
-
 }
